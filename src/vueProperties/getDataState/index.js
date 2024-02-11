af8a180d1f70900bd88c2827df62829cfff4184e
BIPPAN <bippan1407@gmail.com>
2024-02-11 21:56:48 +0530

    update data() property get values

--- src/vueProperties/getDataState/index.js
+++ src/vueProperties/getDataState/index.js
@@ -1,22 +1,40 @@
 const transform = ({ root, j }) => {
   let state = [];
-  root
+  const foundData = root
     .find(j.Property, (path) => {
       if (path.key.name === "data") {
         return true;
       }
       return false;
     })
-    .find(j.ReturnStatement)
+
+  foundData.find(j.ReturnStatement)
     .forEach((path) => {
-      const properties = path.value.argument.properties;
+      const properties = path.value?.argument?.properties;
       if (properties) {
         properties.forEach((property) => {
-          const key = property.key.name;
-          state.push(key);
+          const key = property?.key?.name;
+          if (key) {
+            state.push(key);
+          }
         });
       }
     });
+  if (!state.length) {
+    foundData
+      .find(j.ArrowFunctionExpression)
+      .forEach((path) => {
+        const properties = path.value?.body?.properties;
+        if (properties) {
+          properties.forEach((property) => {
+            const key = property?.key?.name;
+            if (key) {
+              state.push(key);
+            }
+          });
+        }
+      });
+  }
   return state;
 };
 
