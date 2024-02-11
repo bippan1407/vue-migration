499d8660373de9af49c8de31e551cb26b0a0f1d0
BIPPAN <bippan1407@gmail.com>
2024-02-11 21:55:11 +0530

    add head changes

--- src/transformations/convertHead/index.js
+++ src/transformations/convertHead/index.js
@@ -1,16 +1,15 @@
-const transform = ({ root, j, }) => {
-    let headSyntax = ''
-    root
-        .find(j.Property, {
-            key: { name: "head" },
-        })
-        .forEach((path) => {
-            console.log(path)
-            if(path.value.value.body) {
-                headSyntax += `useHead(() => ${j(path.value.value.body).toSource()})`
-            }
-        })
-        return headSyntax;
-}
+const transform = ({ root, j }) => {
+  let headSyntax = "";
+  root
+    .find(j.Property, {
+      key: { name: "head" },
+    })
+    .forEach((path) => {
+      if (path.value.value.body) {
+        headSyntax += `useHead(() => ${j(path.value.value.body).toSource()})`;
+      }
+    });
+  return headSyntax;
+};
 
-module.exports = transform
+module.exports = transform;
